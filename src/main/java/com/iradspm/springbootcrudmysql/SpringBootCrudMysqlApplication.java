3513e4eb0ec9dfaece5dede4a90564ec5ef2f14d
iradspm <iradspm@gmail.com>
2022-04-30 11:29:06 +0300

    Crud Operations with Spring boot-using MYSQL JDBC

---
+++ src/main/java/com/iradspm/springbootcrudmysql/SpringBootCrudMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.iradspm.springbootcrudmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iradspm/springbootcrudmysql/SpringBootCrudMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iradspm.springbootcrudmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
